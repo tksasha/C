00c6798d6915282564708dbfbce363ca96eb552d
Oleksandr Tkachuk <mail@tksasha.me>
2020-10-18 22:32:40 +0300

    implemented Exercise 1-8 from K&R Book

---
+++ src/kre/18.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//
+// Exercise 1.8 from K&R Book
+//
+// Write a program to count blanks, tabs and newlines
+//
+int main() {
+  int c = 0;
+
+  long blanks   = 0;
+  long tabs     = 0;
+  long newlines = 0;
+
+  while((c = getchar()) != EOF) {
+    switch(c) {
+      case ' ':
+        blanks++;
+        break;
+      case '\t':
+        tabs++;
+        break;
+      case '\n':
+        newlines++;
+        break;
+    }
+  }
+
+  printf("blanks  : %ld\n", blanks);
+  printf("tabs    : %ld\n", tabs);
+  printf("newlines: %ld\n", newlines);
+
+  return 0;
+}
